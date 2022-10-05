59ca254f9867a28f1e1d5af5a0f8f727b2c40893
Thais Campos <thaismca.bio@gmail.com>
2022-10-04 17:03:04 -0700

    Form is ready to receive inputs, validate data, and update the formData object.

--- Udemy - Javascript Bootcamp/Events - Form Exercise/card.js
+++ Udemy - Javascript Bootcamp/Events - Form Exercise/card.js
@@ -1,13 +1,26 @@
 //---------- DATA ----------
-const CREDITCARD_TYPES = ['Visa', 'Mastercard', 'AMEX']
-const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+const CREDITCARD_TYPES = ['Visa', 'Mastercard', 'AMEX'];
+const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 const NOW = new Date(Date.now());
 
 //---------- GENERAL FUNCTIONS ----------
 //remove all spaces in strings
 function removeSpaces(str){
     return str = str.replaceAll(' ', '')
+};
+
+//function to reset an object deleting all its properties and methods
+function resetObj(obj){
+    for (const key in obj) {
+        delete obj[key];
+      }
 }
+
+//clear disabled from months
+function resetMonthsSelection(select){
+    for(i=1; i<= 12; i++)
+        select[i].disabled = false;
+};
 
 //accept only numeric inputs
 const isNumericInput = (event) => {
@@ -15,8 +28,8 @@ const isNumericInput = (event) => {
     if(!event.shiftKey) //don't allow input while holding shift
     return ((key >= 48 && key <= 57) || // Allow number line
         (key >= 96 && key <= 105) // Allow number pad
-    );
-}
+    )
+};
 
 //accept only some modifier keys
 const isModifierKey = (event) => {
@@ -24,8 +37,9 @@ const isModifierKey = (event) => {
     if(!event.shiftKey) //don't allow input while holding shift
     return (key === 8 || key === 9 || key === 13 || key === 46) || // Allow Backspace, Tab, Enter, Delete
         (key >= 37 && key <= 40) // Allow left, up, right, down
-}
+};
 
+//accept only letter inputs
 const isNameValidInput = (event) => {
     const key = event.keyCode;
     let test;
@@ -34,7 +48,7 @@ const isNameValidInput = (event) => {
             (key === 45 || key ===32)) // Allow hifen and space   
     else 
         return (key >= 65 && key <= 90) // Allow letters while shift is pressed
-}
+};
 
 //Functions to populate the selection dropdowns
 //first default element that can be applied to all drop down selections
@@ -45,8 +59,8 @@ function defaultSelection(select){
     option.value = removeSpaces(select.name).toLowerCase();
     option.innerText = select.name;
     select.appendChild(option);
-}
-//when elements come from array elements
+};
+//when elements data come from an array
 function populateSelection(arr, select){
     defaultSelection(select);
 
@@ -56,13 +70,12 @@ function populateSelection(arr, select){
         option.innerText = el;
         select.appendChild(option);
     }
-}
+};
 
 //---------- FORM DATA ----------
 const formData = {};
 
 //---------- INPUTS IN THE PAGE ----------
-
 const cardTypeInput = document.querySelector('#cardType');
 const cardNumberInput = document.querySelector('#cardNumber');
 const cardNameInput = document.querySelector('#cardName');
@@ -76,20 +89,21 @@ populateSelection(CREDITCARD_TYPES, cardTypeInput);
 //populating months drop down
 populateSelection(MONTHS, cardMonthInput);
 //populating years drop down
+//adding 10 years counting from the current year
 defaultSelection(cardYearInput);
-//add 10 years counting from the current year
 currYear = NOW.getFullYear();
 for(i=0; i<10; i++){
     const option = document.createElement('option');
     option.value = currYear + i;
     option.innerText = currYear + i;
     cardYearInput.appendChild(option);
-}
+};
 
-
-//---------- CREDIT CARD TYPE - CHECK SELECTION ----------
+//---------- CREDIT CARD TYPE INPUT ----------
 cardTypeInput.addEventListener('input', (e) =>{
-    //populate formData object
+    //clear formData object whenever a new selection for this input is made
+    resetObj(formData);
+    //populate cardType in formData object
     let selectedType = cardTypeInput.value;
     formData['cardType'] = selectedType;
 
@@ -100,7 +114,8 @@ cardTypeInput.addEventListener('input', (e) =>{
     cardNameInput.removeAttribute('disabled');
     cardNameInput.value = '';
     cardMonthInput.removeAttribute('disabled');
-    cardMonthInput.value = 'month'
+    cardMonthInput.value = 'month';
+    resetMonthsSelection(cardMonthInput);
     cardYearInput.removeAttribute('disabled');
     cardYearInput.value = 'year'
     cvvInput.removeAttribute('disabled')
@@ -116,26 +131,68 @@ cardTypeInput.addEventListener('input', (e) =>{
     }
 });
 
-//---------- CREDIT CARD NUMBER - CHECK INPUT ----------
+//---------- CREDIT CARD NUMBER INPUT ----------
 //only register numeric inputs
 cardNumberInput.addEventListener('keydown', (e) => {
-    console.log(e.keyCode)
     if(!isNumericInput(e) && !isModifierKey(e)) e.preventDefault();
     });
-//populate formData object
+
+//populate cardNumber in formData object
 cardNumberInput.addEventListener('input', (e) =>{
     formData['cardNumber'] = e.target.value;  
 });
 
-//---------- CREDIT CARD NAME - CHECK INPUT ----------
-//only register numeric inputs
+//---------- CREDIT CARD NAME INPUT ----------
+//only register name valid inputs
 cardNameInput.addEventListener('keypress', (e) => {
-    console.log(e.keyCode)
     if(!isNameValidInput(e) && !isModifierKey(e)) e.preventDefault();
-    });
-//populate formData object
+});
+
+//populate cardName in formData object
 cardNameInput.addEventListener('input', (e) =>{
     e.target.value = e.target.value.toUpperCase();
     formData['cardName'] = e.target.value;  
 });
 
+//---------- EXIPIRY DATE INPUTS----------
+//year selection input
+cardYearInput.addEventListener('input', (e) => {
+    let selectedYear = cardYearInput.value;
+    //runs when selected year equals the current year
+    if(parseInt(selectedYear) === NOW.getFullYear()){
+        //check selected month and force new selection if already passed in the current year
+        if(parseInt(cardMonthInput.selectedIndex) <= NOW.getMonth()){
+            cardMonthInput.value = 'month'
+            if (formData['cardMonth']) delete formData['cardMonth']
+        }    
+        //disable all month options that already passed in the current year
+        for(i=0; i<= NOW.getMonth(); i++){
+            cardMonthInput[i].disabled = true;
+        }
+    }
+    //runs when selected year is not equal the current year
+    else {
+        //remove disabled state from a possible previous selection of the current year for this input
+        resetMonthsSelection(cardMonthInput);
+    }
+    //populate cardYear in formData object
+    formData['cardYear'] = selectedYear;
+});
+
+//month selection input
+cardMonthInput.addEventListener('input', (e) => {
+    //populate cardMonth in formData object
+    let selectedMonth = cardMonthInput.value;
+    formData['cardMonth'] = selectedMonth;
+});
+
+//---------- CVV INPUT ----------
+//only register numeric inputs
+cvvInput.addEventListener('keydown', (e) => {
+    if(!isNumericInput(e) && !isModifierKey(e)) e.preventDefault();
+});
+
+//populate cvv in formData object
+cvvInput.addEventListener('input', (e) =>{
+    formData['cvv'] = e.target.value;  
+});
