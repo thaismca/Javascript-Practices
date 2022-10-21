f9f6aba81f617f85723dd26c7914aaee7a23f4f0
Thais Campos <thaismca.bio@gmail.com>
2022-10-21 14:36:07 -0700

    Practice Project - Timer add code to stop the countdown when timer reaches 0

--- Udemy - Javascript Bootcamp/Practice Project - Timer/app.js
+++ Udemy - Javascript Bootcamp/Practice Project - Timer/app.js
@@ -11,7 +11,7 @@ class Timer{
         this.playButton.addEventListener('click', this.start);
         this.pauseButton.addEventListener('click', this.pause);
         this.durationInput.addEventListener('focus', this.onChangeDuration);
-    }
+    };
 
     //use arrow functions so the value of 'this' is guaranteed to be be equal to the instance of the class
     //method to run when user clicks the play button
@@ -23,38 +23,45 @@ class Timer{
         //since we are going to need access to the interval outside of the scope of the start function,
         //store a reference to it in an property interval for this instance of Timer
         this.interval = setInterval(this.tick, 1000);
-    }
+    };
 
-    //method to run when user clicks the play button
+    //method to run when user clicks the pause button or when countdown reaches 0
     pause = () => {
         //stop the countdown that was called in start
         clearInterval(this.interval);
-    }
+    };
 
     //method to be called repeatedly over time, as long as the timer is counting down
     //should start when user clicks play, and stop when user clicks pause
     tick = () => {
-        //update the timeRemaining and throw the new value back into the input element using the set and get methods
-        this.timeRemaining = this.timeRemaining - 1;
-    }
+        //check if the countdown reached 0
+        if(this.timeRemaining <= 0){
+            //stop the countdown
+            this.pause();
+        } 
+        else { //still didn't reach 0
+            //update the timeRemaining and throw the new value back into the input element using the set and get methods
+            this.timeRemaining = this.timeRemaining - 1;
+        }
+        
+    };
     //creating getter and setter methods to work with the time remaining in the text input
     get timeRemaining() {
         //reach to the text input and get the current value out of it and parse it into a number
         return parseFloat(this.durationInput.value);
-    }
+    };
     set timeRemaining(time) {
         //set the value of the input to the time parameter
         this.durationInput.value = time;
-    }
+    };
 
     // /called whenever the user clicks the duration text and turns it into a text input
     //so the number can be editted to change the timer duration
     onChangeDuration = () => {
         //pause the timer as soon as the click on the input happens
         this.pause(); 
-    }
-
-}
+    };
+};
 
 //create the references to the DOM elements by selecting them
 const durationInput = document.querySelector('#duration');
