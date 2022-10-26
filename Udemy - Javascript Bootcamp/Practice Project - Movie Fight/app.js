dcc40182cb97cfa3e7afa26a4b20a424a1a13649
Thais Campos <thaismca.bio@gmail.com>
2022-10-25 18:59:47 -0700

    Practice Project - Movie Fight adding information about API in the project outline and creating a first request

--- Udemy - Javascript Bootcamp/Practice Project - Movie Fight/app.js
+++ Udemy - Javascript Bootcamp/Practice Project - Movie Fight/app.js
@@ -1,1 +1,18 @@
-console.log('Hi there!');
+//helper function to make HTTP request
+const fetchData = async () => {
+    //axios.get can receive an object with parameters in the arguments, to create a query string that will be added to the request url
+    const response = await axios.get('http://www.omdbapi.com/', {
+        //according to the API documentation, the apikey and a string s corresponding to a movie title to search for
+        //are the required parameters to make a request using the By Search endpoint
+        params: {
+            apikey: '44e448f2',
+            s: 'avengers'
+        }
+    });
+
+    console.log(response.data);
+};
+
+fetchData();
+
+
