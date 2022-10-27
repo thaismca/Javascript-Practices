cb2a0d6c6bff6e48c05489bbc7106b6c91d9f24b
Thais Campos <thaismca.bio@gmail.com>
2022-10-26 17:07:50 -0700

    Practice Project - Movie Fight Moving the generation of the HTML following the required structure for a dropdown using bulma to the js file, and also updating the onInput function to work with the new structure to append results properly.

--- Udemy - Javascript Bootcamp/Practice Project - Movie Fight/app.js
+++ Udemy - Javascript Bootcamp/Practice Project - Movie Fight/app.js
@@ -18,27 +18,53 @@ const fetchData = async (searchTerm) => {
     return response.data.Search;
 };
 
+//moving the generation of HTML following the required structure for a dropdown using bulma to the js file
+//this allows a more independent code for a more reusable widget, that can be applied to any HTML file
+//requiring only one div existing in the HTML document where this HTML can be added to work, not the entire structure
+//in this case, this one existing div in the HTML document must have an "autocomplete" class applied to it
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+    <label>Search for a movie</label>
+    <input class="input" />
+    <div class="dropdown">
+        <div class="dropdown-menu">
+          <div class="dropdown-content results">
+          </div>
+        </div>
+      </div>
+`;
+
+//select the elements from the autocomplete that we are going to need to work with 
+const input = document.querySelector('.input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.dropdown-content');
+
+
 //a function that can be called on user input
 //assign the function to a variable that will be passed as the second argument in the input EventListener
 const onInput = async (e) => {
     //store whatever returns from fetchData to a variable, using await because fetchData is async
     const movies = await fetchData(e.target.value);
 
+    //make the dropdown visible by adding class is-active (from Bulma structure) to the element selected and stored in the const dropdown
+    dropdown.classList.add('is-active');
+
     //iterate over the list of movies that came back from the request
     for(let movie of movies) {
-        //for every movie that was fetched, create a div element that sumarizes the movie (poster image and string with title + year)
-        const div = document.createElement('div');
-        div.innerHTML = `
+        //for every movie that was fetched, create a anchor tag element that sumarizes the movie (poster image and string with title + year)
+        //this tag must have the dropdown-item class applied for styling purposes (from Bulma structure)
+        const listOption = document.createElement('a');
+        listOption.classList.add('dropdown-item');
+        listOption.innerHTML = `
             <img src="${movie.Poster}"/>
-            <h1>${movie.Title} (${movie.Year})</h1>
+            ${movie.Title} (${movie.Year})
         `;
-        //append this new div to the document
-        document.querySelector('#temp').appendChild(div);
+        //append this new anchor tag that represents a list option that can be selected in the dropdown to the resultsWrapper
+        resultsWrapper.appendChild(listOption);
     }
 };
 
-//select the search input and add the event listener
-const input = document.querySelector('input');
+//add the event listener to the selected input
 //wrap the onInput function into the debounce helper function, with a delay of 500 miliseconds
 input.addEventListener('input', debounce(onInput, 500));
 
