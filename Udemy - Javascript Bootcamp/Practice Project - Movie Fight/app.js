fefaa445a1b632d73eb2d9c2a2cd87dba2bebf9e
Thais Campos <thaismca.bio@gmail.com>
2022-10-29 16:09:17 -0700

    Practice Project - Movie Fight Code refactoring part #4: removing movie references and reorganizing code files to separate what pertains to autocomplete and what is part of the widget customization that only applies to the movie search using OMDb API

--- Udemy - Javascript Bootcamp/Practice Project - Movie Fight/app.js
+++ Udemy - Javascript Bootcamp/Practice Project - Movie Fight/app.js
@@ -1,8 +1,40 @@
+//----------CUSTOMIZE AUTOCOMPLETE-----------------------------------------------------------------------------------------------------------
+//This function call creates an autocomplete widget using the template declared in autocomplete.js
+//it receives the following parameters
+//dropdownRoot: property that assigns the element where this dropdown will be inserted
+//searchRequest: function to make a HTTP request that returns a list of options to be displayed, according to the search term,
+//or an error message, if no results return from the search (assigned to property searchError)
+//renderOption: function that knows how to render an option in the dropdown
+//onOptionSelect: function that knows what should be done when a option is selected
+//setInputValue: function to set what information must be displayed in the input when an option is selected
 createAutocomplete({
     //property that defines the element that the autocomplete should be added to in the page
     dropdownRoot: document.querySelector(".autocomplete-one"),
-    
-    //function that returns a string containing HTML that renders a movie option
+
+    //function to make a HTTP request to the OMDb API By Search
+    //must return either a list of results or an error message in the searchError property
+    async searchRequest(searchTerm) {
+        //axios.get can receive an object with parameters in the arguments, to create a query string that will be added to the request url
+        const response = await axios.get('http://www.omdbapi.com/', {
+        //according to the API documentation, the apikey and a string s corresponding to a movie title to search for
+        //are the required parameters to make a request using the By Search endpoint
+        params: {
+            apikey: '44e448f2',
+            s: searchTerm
+        }
+    });
+
+    if(response.data.Error){
+        this.searchError = response.data.Error; //creates a property to hold info about error (no results are found or too many results)
+        return; 
+    }
+    //return the data form the response that is relevant to this application
+    //Search is a property inside of the response object that is an array of objects that contain information about the search results
+    this.searchError = ''; //clear searchError from a previous search
+    return response.data.Search;
+},
+
+    //function that returns a string containing HTML that renders a movie option containing a poster image, movie title and year
     renderOption(movie) {
         //some movies from the API don't have a link to the image in the Poster property, they have the string 'N/A' instead
         //handle so there are no broken references in our list if the movie doesn't actually have a poster
@@ -16,14 +48,74 @@ createAutocomplete({
 
     //function to be invoked when a option in the dropdown menu is select
     async onOptionSelect(movie) {
-        //make follow up request for movie details
+        //use helper function to make follow up request for movie details
         const movieDetail = await movieRequest(movie);
-        //use the helper function to create the HTML using the movie template and add this to the HTML document
+        //use helper function to create the HTML using the movie template and add this to the HTML document
         document.querySelector('#summary').innerHTML = movieTemplate(movieDetail);
     },
-
     //function to display the selected movie name and year inside of the input once an option is selected
     setInputValue(movie) {
         return `${movie.Title} (${movie.Year})`;
     }
-})
+});
+
+//----------HELPER FUNCTIONS-----------------------------------------------------------------------------------------------------------------
+//helper function to make a follow up HTTP request to the OMDb API By ID
+//it accepts a movie object that will have this imdbID property to be past as the i argument expected by the API
+const movieRequest = async (movie) => {
+    //axios.get can receive an object with parameters in the arguments, to create a query string that will be added to the request url
+    const response = await axios.get('http://www.omdbapi.com/', {
+        //according to the API documentation, the apikey and a string i corresponding to the movie IMDb ID
+        //are the required parameters to make a request using the By ID endpoint
+        params: {
+            apikey: '44e448f2',
+            i: movie.imdbID
+        }
+    });
+    //return the data form the response that is relevant to this application
+    //Search is a property inside of the response object that is an array of objects that contain information about the search results
+    return response.data;
+};
+//--------------------------------------------------------------------------------------------------------------------------------------------
+//helper to render the HTML that displays a movie details, using classes from bulma to style how information is displayed
+const movieTemplate = (movieDetail) => {
+    return `
+        <!--movie summary-->
+        <article class="media">
+          <figure class="media-left">
+            <p class="image">
+              <img src="${movieDetail.Poster}" />
+            </p>
+          </figure>
+          <div class="media-content">
+            <div class="content">
+              <h1>${movieDetail.Title}</h1>
+              <h4>${movieDetail.Genre}</h4>
+              <p>${movieDetail.Plot}</p>
+            </div>
+          </div>
+        </article>
+
+        <!--movie stats-->
+        <article class="notification is-primary">
+          <p class="title">${movieDetail.Awards}</p>
+          <p class="subtitle">Awards</p>
+        </article>
+        <article class="notification is-primary">
+          <p class="title">${movieDetail.BoxOffice}</p>
+          <p class="subtitle">Box Office</p>
+        </article>
+        <article class="notification is-primary">
+          <p class="title">${movieDetail.Metascore}</p>
+          <p class="subtitle">Metascore</p>
+        </article>
+        <article class="notification is-primary">
+          <p class="title">${movieDetail.imdbRating}</p>
+          <p class="subtitle">IMDb Rating</p>
+        </article>
+        <article class="notification is-primary">
+          <p class="title">${movieDetail.imdbVotes}</p>
+          <p class="subtitle">IMDb Votes</p>
+        </article>
+    `;
+}
--- Udemy - Javascript Bootcamp/Practice Project - Movie Fight/autocomplete.js
+++ Udemy - Javascript Bootcamp/Practice Project - Movie Fight/autocomplete.js
@@ -1,10 +1,12 @@
-//a function to create a reusable autocomplete widget
+//declaring a function to create a reusable autocomplete widget
 //it receives a config object containing all the kind of custom functions that specify how autocompete should work for a given project
 //dropdownRoot: the element where this dropdown will be inserted
+//searchRequest: function to make a HTTP request that returns a list of options to be displayed, according to the search term,
+//or an error message, if no results return from the search
 //renderOption: function that knows how to render an option in the dropdown
 //onOptionSelect: function that knows what should be done when a option is selected
 //setInputValue: function to set what information must be displayed in the input when an option is selected
-const createAutocomplete = ({dropdownRoot, renderOption, onOptionSelect, setInputValue}) => {
+const createAutocomplete = ({dropdownRoot, searchRequest, renderOption, onOptionSelect, setInputValue}) => {
     //generation of HTML following the required structure for a dropdown using bulma to the js file
     //this allows a more independent code for a more reusable widget, that can be applied to any HTML file
     //the element where this dropdown will be inserted will be passed in the config object as dropdownRoot
@@ -32,40 +34,39 @@ const createAutocomplete = ({dropdownRoot, renderOption, onOptionSelect, setInpu
             dropdown.classList.remove('is-active');
             return;
         }
-        //store whatever returns from fetchData to a variable, using await because fetchData is async
-        const movies = await searchRequest(e.target.value);
-        console.log(movies)
+        
+        const searchResults = await searchRequest(e.target.value);
 
         //make sure to clean any existing result list from a previous search request
         resultsWrapper.innerHTML = ""; 
         //make the dropdown visible by adding class is-active (from Bulma structure) to the element selected and stored in the const dropdown
         dropdown.classList.add('is-active');
 
-        //if there's an Error property in the movies object, display the error message and don't execute anything else
-        if(movies.Error){
-            resultsWrapper.innerHTML = `<i>${movies.Error}</i>`
+        //if there's a message assigned to searchError, display the error message and don't execute anything else
+        if(searchError){
+            resultsWrapper.innerHTML = `<i> ${searchError} </i>`;
             return;
         }
 
-        //iterate over the list of movies that came back from the request
-        for(let movie of movies) {
-            //for every movie that was fetched, create a anchor tag element that sumarizes the movie (poster image and string with title + year)
+        //iterate over the list of results of a successful search that came back from the request
+        for(let result of searchResults) {
+            //for every result that was fetched, create a anchor tag element
             //this tag must have the dropdown-item class applied for styling purposes (from Bulma structure)
             const listOption = document.createElement('a');
             listOption.classList.add('dropdown-item');
             //create option using the HTML returned from renderOption, passing the current item that we are at from the iterable
-            listOption.innerHTML = renderOption(movie);
+            listOption.innerHTML = renderOption(result);
 
-            //add an event listener to the listOption to listen for a click on it (when user selects a movie in the dropdown)
+            //add an event listener to the listOption to listen for a click on it (when user selects a option in the dropdown)
             listOption.addEventListener('click', async (e) => {
             //when the item is clicked
                 //close the dropdown menu
                 dropdown.classList.remove('is-active');
                 //update the text inside the input to match the selection
-                input.value = setInputValue(movie);
+                input.value = setInputValue(result);
                 //call the function that was passed in the config that should be called when a option is selected
                 //passing the current item that we are at from the iterable
-                onOptionSelect(movie);
+                onOptionSelect(result);
             });
 
             //append this new anchor tag that represents a list option that can be selected in the dropdown to the resultsWrapper
@@ -85,8 +86,28 @@ const createAutocomplete = ({dropdownRoot, renderOption, onOptionSelect, setInpu
             dropdown.classList.remove('is-active');
         }
     });
+}
 
-
-
-
-    }
+//helper debounce function that receives a function in the arguments, to be used anywhere in the code where we want to introduce
+//some rate limiting on how often this function can be invoked
+//it also receives a delay argument, that represents the miliseconds to be passed in the setTimeout (default to 1000)
+const debounce = (func, delay = 1000) => {
+    //it will return a function that will implement a shield and guard how ofter func can actually be invoked
+    //func might need to receive some arguments, so we need to make sure that if we ever pass any arguments to this wrapping function
+    //they will be forward to func whenever it's called
+    let timeoutId; //variable to keep track of the current timer identifier
+    return (...args) => {
+        //check if there is a timer from a previous input that is still pending
+        if(timeoutId) {
+            //if one is found, clearTimeout so func is never called
+            clearTimeout(timeoutId);
+        }
+        //wait one second to run the function passed in the argument of debounce()
+        //and assign the value that is returned from the setTimeout to the timeoutId variable
+        timeoutId = setTimeout(() => {
+            //call the function and take all the arguments or whatever is inside of the args array and pass them in
+            //as separate arguments to the original function
+            func.apply(null, args);
+        }, delay);
+    };
+};
