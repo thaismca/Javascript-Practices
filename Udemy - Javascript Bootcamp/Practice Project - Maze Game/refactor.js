7f295597c67ccba5bcd42082529aabaf89efb3cd
Thais Campos <thaismca.bio@gmail.com>
2022-11-07 12:31:34 -0800

    Practice Project - Maze Game/index.html starting to add code to experiment with multiple levels

--- Udemy - Javascript Bootcamp/Practice Project - Maze Game/refactor.js
+++ Udemy - Javascript Bootcamp/Practice Project - Maze Game/refactor.js
@@ -2,11 +2,22 @@
 //maze config
 const mazeWidth = document.documentElement.clientWidth;
 const mazeHeight = document.documentElement.clientHeight;
-const mazeRows = 3;
-const mazeColumns = 3;
+let mazeRows = 3;
+let mazeColumns = 3;
 
 //start the game
 let maze = gameStart(mazeWidth, mazeHeight, mazeRows, mazeColumns);
 
 //play again
 document.querySelector('#restart').addEventListener('click', reset);
+
+//change
+document.querySelector('#next').addEventListener('click', () =>{
+    if(mazeRows === mazeColumns){
+        mazeColumns++;
+    } else {
+        mazeRows++;
+    }
+    reset();
+});
+
