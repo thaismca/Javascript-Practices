30b2e7638c6b69e03f5747be943ab633e2d3f183
Thais Campos <thaismca.bio@gmail.com>
2022-11-10 14:54:26 -0800

    PracticeProject - Secret Message Sharing added challenge: add limit to the message length and a counter that keeps track of the characters that the user can still type

--- Udemy - Javascript Bootcamp/PracticeProject - Secret Message Sharing/app.js
+++ Udemy - Javascript Bootcamp/PracticeProject - Secret Message Sharing/app.js
@@ -3,6 +3,8 @@ const messageForm = document.querySelector('#message-form');
 const linkForm = document.querySelector('#link-form');
 const messageDisplay = document.querySelector("#message-display");
 
+//----- DISPLAY DECODED MESSAGE ------------------------------------------------------------------------------------------------
+
 //checking if the url that is being visited already contains a hash ->which means it carries an encoded message
 const { hash } = window.location;
 if(hash) {
@@ -16,6 +18,27 @@ if(hash) {
     document.querySelector('#message-decoded').innerText = decoded;
 }
 
+//----- MESSAGE INPUT AND LINK GENERATION ---------------------------------------------------------------------------------------
+
+//create a reference to the input where the message is typed
+const messageInput = document.querySelector('#message-input');
+//create a reference to the chars counter
+const charsCounter = document.querySelector('#chars-counter');
+
+//set a character limit for the message
+const limit = 75;
+messageInput.setAttribute('maxlength', limit);
+//a variable to hold how many chars user can still type -> initialized with whatever the limit is
+let charsLeft = limit;
+charsCounter.innerText = `${charsLeft}/${limit}`
+
+//handling characters counter
+messageInput.addEventListener('input', (e) => {
+    //update how many chars user can still type
+    charsLeft = limit - messageInput.value.length;
+    //display updated information
+    charsCounter.innerText = `${charsLeft}/${limit}`
+});
 
 //handling message form submission
 document.querySelector('form').addEventListener('submit', (e) => {
