b220d825c4b850ce65ea5e91188bad897d203448
Thais Campos <thaismca.bio@gmail.com>
2022-11-10 11:57:48 -0800

    PracticeProject - Secret Message Sharing decoding and displaying the message

--- Udemy - Javascript Bootcamp/PracticeProject - Secret Message Sharing/app.js
+++ Udemy - Javascript Bootcamp/PracticeProject - Secret Message Sharing/app.js
@@ -1,7 +1,30 @@
-//handling form submission
+//create references to relevant elements in the page
+const messageForm = document.querySelector('#message-form');
+const linkForm = document.querySelector('#link-form');
+const messageDisplay = document.querySelector("#message-display");
+
+//checking if the url that is being visited already contains a hash ->which means it carries an encoded message
+const { hash } = window.location;
+if(hash) {
+    //toggle elements visibility
+    messageForm.classList.add('hide');
+    messageDisplay.classList.remove('hide');
+    
+    //decode the part of the hash that represents the encoded message (the hash without the # symbol)
+    const decoded = atob(hash.replace('#', ''));
+    //add the message to the document
+    document.querySelector('#message-decoded').innerText = decoded;
+}
+
+
+//handling message form submission
 document.querySelector('form').addEventListener('submit', (e) => {
     //prevent the form submission to a backend server
     e.preventDefault();
+
+    //toggle elements visibility
+    messageForm.classList.add('hide');
+    linkForm.classList.remove('hide');
 
     //get a reference to the input where the message is typed
     const messageInput = document.querySelector('#message-input');
@@ -10,14 +33,8 @@ document.querySelector('form').addEventListener('submit', (e) => {
 
     //get a reference for the input where the link is displayed
     const linkInput = document.querySelector('#link-input');
-
     //set its value a string formed by the current window.location + the encoded string as the hash part of the URL
     linkInput.value = `${window.location}#${encoded}`;
     //automatically select the text so the user can easily copy it to the clipboard
     linkInput.select();
-
-    //toggle elements visibility
-    document.querySelector('#message-form').classList.add('hide');
-    document.querySelector('#link-form').classList.remove('hide');
-    
 });
