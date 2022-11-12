90774680f4a738af8c85ac1023470db7320aefbc
Thais Campos <thaismca.bio@gmail.com>
2022-11-12 12:54:33 -0800

    Practice Project - NodeJS Command Line Tools working with process.cwd method

--- Udemy - Javascript Bootcamp/Practice Project - NodeJS Command Line Tools/app.js
+++ Udemy - Javascript Bootcamp/Practice Project - NodeJS Command Line Tools/app.js
@@ -6,7 +6,7 @@ const fs = require('node:fs');
 //so now in this object we can get access to every single function listed in the File System module documentation
 //the one we care about for the sake of this app is the readdir function, that reads the contents of a directory
 
-fs.readdir('.', (err, files) => {
+fs.readdir(process.cwd(), (err, files) => {
     //the callback gets two arguments (err, files) where files is an array of the names of the files in the directory
 
     //EITHER err === an error object, which means something wen wrong
