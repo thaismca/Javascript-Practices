0ac91c92011714c152bcf4e74031ccc973ebe92c
Thais Campos <thaismca.bio@gmail.com>
2022-11-14 18:06:19 -0800

    Practice Project - NodeJS Command Line Tools accepting command line arguments and adding a colour differentiation for directories

--- Udemy - Javascript Bootcamp/Practice Project - NodeJS Command Line Tools/app.js
+++ Udemy - Javascript Bootcamp/Practice Project - NodeJS Command Line Tools/app.js
@@ -6,6 +6,9 @@
 //get access to the File System Module from Node.js inside of this project
 //The node:fs module enables interacting with the file system
 const fs = require('node:fs');
+//get access to the Path Module from Node.js inside of this project
+//The node:path module provides utilities for working with file and directory paths
+const path = require('node:path');
 //get access to the chalk package
 const chalk = require('chalk');
 
@@ -15,7 +18,12 @@ const chalk = require('chalk');
 //using lstat version from the fs Promises API method
 const lstat = fs.promises.lstat; //alternatively const { lstat } = fs.promises
 
-fs.readdir(process.cwd(), async (err, files) => {
+//determine the directory that will have its contents print out
+//process.argv[2] not null means an argument was passed -> print out the content from the directory that was passed
+//process.argv[2] null means no argument was passed -> print out the content from the current directory
+const targetDir = process.argv[2] || process.cwd();
+
+fs.readdir(targetDir, async (err, files) => {
     //the callback gets two arguments (err, files) where files is an array of the names of the files in the directory
 
     //EITHER err === an error object, which means something wen wrong
@@ -26,9 +34,10 @@ fs.readdir(process.cwd(), async (err, files) => {
     }
 
     //PROMISE.ALL BASED SOLUTION
-    //map over the files array and call lstat for each file -> create an array with the Promises that are returned from each lstat call
+    //map over the files array and call lstat for each file in the target directory
+    //this will create an array with the Promises that are returned from each lstat call
     const statPromises = files.map((file) => {
-        return lstat(file);
+        return lstat(path.join(targetDir, file));
     });
 
     //pass the statPromises array to Promise.all -> when all promises from the array are resolved,
@@ -44,10 +53,15 @@ fs.readdir(process.cwd(), async (err, files) => {
             //print out the element in files that matches the current allStats index, and color file name in yellow 
             console.log(chalk.yellow(files[index]));
         }
-        //current element is not a file
-        else {
+        //current element is a directory
+        else if(stats.isDirectory()){
             //print out the element in files that matches the current allStats index, and color its name in bold green 
             console.log(chalk.green.bold(files[index]));
+        }
+        //current element is neither a file nor a directory
+        else{
+            //print out the element in files that matches the current allStats index, and color its name in white 
+            console.log(chalk.white(files[index]));
         }
     }
     
