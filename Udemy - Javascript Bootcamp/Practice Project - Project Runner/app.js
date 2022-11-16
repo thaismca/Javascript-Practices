3bf358999cc7c80aaef648c5e50a92bc1ff7fb5a
Thais Campos <thaismca.bio@gmail.com>
2022-11-15 17:15:32 -0800

    Practice Project - Project Runner debouncing 'add' events and fixing syntax for 'unlink' event

--- Udemy - Javascript Bootcamp/Practice Project - Project Runner/app.js
+++ Udemy - Javascript Bootcamp/Practice Project - Project Runner/app.js
@@ -3,15 +3,19 @@
 //get access to the chokidar package inside of this project
 //package that will be used to detect when file changes, are added or unliked
 const chokidar = require('chokidar');
+const debounce = require('lodash.debounce');
+
+//a function to be executed when chokidar emits a 'add' event
+//debounce the function so it doesn't get called to often upon program start
+const start = debounce(() => {
+    console.log('STARTING USERS PROGRAM');
+}, 100);
 
 //use chokidar to watch the current directory
 //instead of chain on an event listener to watch for all event types covered by chokidar
 //add listeners for when a file is added, changed or unlinked from the directory
-chokidar.watch('.')
-    .on('add', () => console.log('FILE ADDED'))
+chokidar
+.watch('.')
+    .on('add', start)
     .on('change', () => console.log('FILE CHANGED'))
-    .on('unlinked', () => console.log('FILE UNLINKED'));
-
-//ISSUE WITH ADD EVENT: the 'add' callback is not only going to be invoked whenever a new file is created inside of the project 
-//directory, but it will also be called when chokidar first starts up and sees a file inside of the project direcory. So when 
-//the program is started we immediately see a tons of different 'FILE ADDED' console logs statements.
+    .on('unlink', () => console.log('FILE UNLINKED'));
