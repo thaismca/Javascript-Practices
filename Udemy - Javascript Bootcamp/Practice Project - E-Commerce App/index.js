02591769bc9b33440dfd2ffba9e330691036c3b1
Thais Campos <thaismca.bio@gmail.com>
2022-11-18 14:46:08 -0800

    Practice Project - E-Commerce App Starting to create a web server using express

--- Udemy - Javascript Bootcamp/Practice Project - E-Commerce App/index.js
+++ Udemy - Javascript Bootcamp/Practice Project - E-Commerce App/index.js
@@ -1,1 +1,16 @@
-console.log('testing project setup...');
+//get access to the express package inside of this project
+//package that will be used to help creating a web server
+const express = require('express');
+//create an instance of express
+//this object describes all the different thing that the web server can do
+const app = express();
+
+//route handler that tells the web server what it should do when it receives a network request coming from the browser
+app.get('/', (req, res) => {
+    res.send('hi there');
+});
+
+//start listening fom incoming network traffic on a particular port
+app.listen(3000, () => {
+    console.log('listening');
+});
