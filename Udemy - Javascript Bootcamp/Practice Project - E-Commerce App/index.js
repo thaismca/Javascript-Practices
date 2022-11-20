047e5b233eb18ba2fc976208d3f2fd1f87a51f8d
Thais Campos <thaismca.bio@gmail.com>
2022-11-20 15:35:43 -0800

    Practice Project - E-Commerce App manually parsing Admin Sign Up form data

--- Udemy - Javascript Bootcamp/Practice Project - E-Commerce App/index.js
+++ Udemy - Javascript Bootcamp/Practice Project - E-Commerce App/index.js
@@ -23,6 +23,29 @@ app.get('/', (req, res) => {
 
 //watching for incoming requests for a path of '/' and a method of POST
 app.post('/', (req, res) => {
+    //get access to info from admin sign up form (email, password, passwordConfirmation)
+
+    //the request object emits a data event any time that it receives some bit of data
+    //listening for this data event, and then getting access to the data that is being received
+    req.on('data', data => {
+        //an object that is going to eventually contain all the information from the request body
+        const formData = {};
+
+        //data comes as a buffer, so parse it to a utf-8 encoded string to make it readable
+        //split at '&', to get an array of strings with 'email=...', 'password=...' and 'passwordConfirmation=...'
+        const parsed = data.toString('utf8').split('&');
+     
+        //loop over the array
+        for(let pair of parsed){
+            //split each array element at '=' to get pair of key and value
+            const [key, value] = pair.split('=');
+            //use key and value to add that information to the formData object
+            formData[key] = value;
+        }
+        //display formData object in the log
+        console.log(formData);
+    });
+
     res.send('account created');
 });
 
