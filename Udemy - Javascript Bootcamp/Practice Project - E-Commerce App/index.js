a21b7a5368580170f224d6ad383e7d670e11d9bb
Thais Campos <thaismca.bio@gmail.com>
2022-11-20 14:50:32 -0800

    Practice Project - E-Commerce App starting to work on the Admin Sign Up form, and getting more understanding on how form submissions work

--- Udemy - Javascript Bootcamp/Practice Project - E-Commerce App/index.js
+++ Udemy - Javascript Bootcamp/Practice Project - E-Commerce App/index.js
@@ -5,10 +5,27 @@ const express = require('express');
 //this object describes all the different thing that the web server can do
 const app = express();
 
-//route handler that tells the web server what it should do when it receives a network request coming from the browser
+//route handlers that tells the web server what it should do when it receives a network request coming from the browser
+//watching for incoming requests for a path of '/' and a method of GET 
 app.get('/', (req, res) => {
-    res.send('hi there');
+    //adminstrator sign up form
+    res.send(`
+        <div>
+          <form method="POST">
+            <input name="email" placeholder="email" />
+            <input name="password" placeholder="password" />
+            <input name="passwordConfirmation" placeholder="password confirmation" name />
+            <button>Sign Up</button>
+          </form>
+        </div>
+    `);
 });
+
+//watching for incoming requests for a path of '/' and a method of POST
+app.post('/', (req, res) => {
+    res.send('account created');
+});
+
 
 //start listening fom incoming network traffic on a particular port
 app.listen(3000, () => {
