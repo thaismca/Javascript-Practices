a6d125057da0d812530fca357bc36e5da3b70f20
Thais Campos <thaismca.bio@gmail.com>
2022-11-21 12:44:05 -0800

    Practice Project - E-Commerce App/index first implementation of a middleware function to parse the data from the body of an incoming POST request

--- Udemy - Javascript Bootcamp/Practice Project - E-Commerce App/index.js
+++ Udemy - Javascript Bootcamp/Practice Project - E-Commerce App/index.js
@@ -21,30 +21,41 @@ app.get('/', (req, res) => {
     `);
 });
 
+//helper function to parse the information in the body of a POST request
+const bodyParser = (req, res, next) => {
+    //check if this is a POST request
+    if(req.method === 'POST'){
+        //the request object emits a data event any time that it receives some bit of data
+        //listening for this data event, and then getting access to the data that is being received
+        req.on('data', data => {
+            //an object that is going to eventually contain all the information from the request body
+            const formData = {};
+
+            //data comes as a buffer, so parse it to a utf-8 encoded string to make it readable
+            //split at '&', to get an array of strings with 'email=...', 'password=...' and 'passwordConfirmation=...'
+            const parsed = data.toString('utf8').split('&');
+        
+            //loop over the array
+            for(let pair of parsed){
+                //split each array element at '=' to get pair of key and value
+                const [key, value] = pair.split('=');
+                //use key and value to add that information to the formData object
+                formData[key] = value;
+            }
+            //take the formData object and assign it to the body property of the request
+            req.body = formData;
+            //sinalize that the middleware function is all done with processing
+            next();
+        });
+    } else { //not a POST request, so we don't care about processing it in here
+        next();
+    }
+};
+
 //watching for incoming requests for a path of '/' and a method of POST
-app.post('/', (req, res) => {
-    //get access to info from admin sign up form (email, password, passwordConfirmation)
-
-    //the request object emits a data event any time that it receives some bit of data
-    //listening for this data event, and then getting access to the data that is being received
-    req.on('data', data => {
-        //an object that is going to eventually contain all the information from the request body
-        const formData = {};
-
-        //data comes as a buffer, so parse it to a utf-8 encoded string to make it readable
-        //split at '&', to get an array of strings with 'email=...', 'password=...' and 'passwordConfirmation=...'
-        const parsed = data.toString('utf8').split('&');
-     
-        //loop over the array
-        for(let pair of parsed){
-            //split each array element at '=' to get pair of key and value
-            const [key, value] = pair.split('=');
-            //use key and value to add that information to the formData object
-            formData[key] = value;
-        }
-        //display formData object in the log
-        console.log(formData);
-    });
+app.post('/', bodyParser, (req, res) => {
+    //display information from the body property of the req object
+    console.log(req.body);
 
     res.send('account created');
 });
