67ed150921455cc36ee7c6b231fb84c5bf3406d0
Thais Campos <thaismca.bio@gmail.com>
2022-11-22 16:00:45 -0800

    Practice Project - E-Commerce App creating UsersRepository class, and defining its constructor

---
+++ Udemy - Javascript Bootcamp/Practice Project - E-Commerce App/repositories/users.js
@@ -0,0 +1,30 @@
+//get access to the File System Module from Node.js inside of this project
+//The node:fs module enables interacting with the file system
+const fs = require('node:fs');
+
+class UsersRepository {
+    //constructor to create an instance of the UsersRepository
+    //it receives a filename that represents where the info is supposed to be saved when an instance of the repository is created
+    constructor(filename) {
+        //check if a filename was passed in the arguments
+        if(!filename) {
+            //no filename -> no way to know where to save information
+            throw new Error('Creating a repository requires a filename');
+        }
+
+        //take whatever filename that was provided and store it in a instance variable
+        this.filename = filename;
+        //check if a file with that given name already exists
+        try {
+            fs.accessSync(this.filename);
+        } 
+        //block that runs if file with that given name does not exist
+        catch {
+            //create the file
+            fs.writeFileSync(this.filename, '[]');
+        }
+    }
+}
+
+//testing code
+new UsersRepository('users.json');
