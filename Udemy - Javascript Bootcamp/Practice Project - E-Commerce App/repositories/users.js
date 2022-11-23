37361939533ee4c2db3d59badaecd43235359965
Thais Campos <thaismca.bio@gmail.com>
2022-11-22 17:20:18 -0800

    Practice Project - E-Commerce App implementing method to create a new user

--- Udemy - Javascript Bootcamp/Practice Project - E-Commerce App/repositories/users.js
+++ Udemy - Javascript Bootcamp/Practice Project - E-Commerce App/repositories/users.js
@@ -32,11 +32,24 @@ class UsersRepository {
         //return the parsed data
         return JSON.parse(await fs.promises.readFile(this.filename, { encoding: 'utf8' }));
     }
+
+    //method to create a new user
+    //it receives an object with an user's attributes
+    async create(attrs) {
+        //load up the array of records inside the current version of this.filename
+        const records = await this.getAll();
+        //add in the new user to the array
+        records.push(attrs);
+        //write updated array of records back to this.filename in JSON string format (utf8 encoding option is the default)
+        await fs.promises.writeFile(this.filename, JSON.stringify(records));
+    }
 }
 
 //testing code
 const test = async () => {
     const repo = new UsersRepository('users.json');
+    await repo.create({email: 'test1@test.com', password: 'testing1'});
+    await repo.create({email: 'test2@test.com', password: 'testing2'});
     const users = await repo.getAll();
     console.log(users);
 }
