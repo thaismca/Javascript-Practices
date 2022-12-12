ff7720e8111ec38b0b4d2010140548636246bead
Thais Campos <thaismca.bio@gmail.com>
2022-12-11 19:57:28 -0800

    Practice Project - E-Commerce App Initial implementation of a new product form

--- Udemy - Javascript Bootcamp/Practice Project - E-Commerce App/routes/admin/products.js
+++ Udemy - Javascript Bootcamp/Practice Project - E-Commerce App/routes/admin/products.js
@@ -3,6 +3,9 @@ const express = require('express');
 //require an instance of the ProductsRepository class
 const productsRepo = require('../../repositories/products');
 
+//get access to views from other files in this project
+const productNewTemplate = require('../../views/admin/products/new');
+
 //create an instance an router object from the express library
 const router = express.Router();
 
@@ -17,7 +20,7 @@ router.get('/admin/products', (req, res) => {
 //watching for incoming requests for a path of '/admin/products/new' and a method of GET
 //display a form to create a new product
 router.get('/admin/products/new', (req, res) => {
-    //code that will display a form to create a new product
+    res.send(productNewTemplate({ }));
 });
 
 //watching for incoming requests for a path of '/admin/products/new' and a method of POST
---
+++ Udemy - Javascript Bootcamp/Practice Project - E-Commerce App/views/admin/products/new.js
@@ -0,0 +1,21 @@
+//Exports HTML that generates new product form
+//it reuses the layout created for the admin panel
+const layout = require('../layoutAdmin.js');
+
+//get access to helper functions file and destruct out the getError method
+//helper function to get potential errors in new product form validation
+const { getError } = require('../../helpers.js');
+
+module.exports = ({ errors }) => {
+  //pass the content of the new product page to layout
+  return layout({
+    content: `
+      <form method="POST">
+        <input placeholder="Product name" name="productName" />
+        <input placeholder="Price" name="price" />
+        <input type="file" name="image" />
+        <button>Create</button
+      </form>
+    `
+  });
+};
