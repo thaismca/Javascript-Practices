cc4f088af899d30f2fc486796be254f50d52bc2d
Thais Campos <thaismca.bio@gmail.com>
2022-12-12 18:31:27 -0800

    Practice Project - E-Commerce App Adding classes to the new product form, for styling purposes.

--- Udemy - Javascript Bootcamp/Practice Project - E-Commerce App/views/admin/products/new.js
+++ Udemy - Javascript Bootcamp/Practice Project - E-Commerce App/views/admin/products/new.js
@@ -10,14 +10,30 @@ module.exports = ({ errors }) => {
   //pass the content of the new product page to layout
   return layout({
     content: `
-      <form method="POST" enctype="multipart/form-data">
-        <input placeholder="Product name" name="productName" />
-        ${getError(errors, 'productName')}
-        <input placeholder="Price" name="price" />
-        ${getError(errors, 'price')}
-        <input type="file" name="image" />
-        <button>Create</button
-      </form>
+      <div class="columns is-centered">
+        <div class="column is-half">
+          <h1 class="subtitle">Create a Product</h1>
+
+          <form method="POST" enctype="multipart/form-data">
+            <div class="field">
+              <label class="label">Title</label>
+              <input class="input" placeholder="Product Name" name="productName">
+              <p class="help is-danger">${getError(errors, 'productName')}</p>
+            </div>
+            <div class="field">
+              <label class="label">Price</label>
+              <input class="input" placeholder="Price" name="price">
+              <p class="help is-danger">${getError(errors, 'price')}</p>
+            </div>
+            <div class="field">
+              <label class="label">Image</label>            
+              <input type="file" name="image" />
+            </div>
+            <br />
+            <button class="button is-primary">Create</button>
+          </form>
+        </div>
+      </div>
     `
   });
 };
