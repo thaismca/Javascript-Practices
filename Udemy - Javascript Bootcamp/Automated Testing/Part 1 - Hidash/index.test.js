53b8c3003a72dccfb0df61e813718e5ef2b4d6b1
Thais Campos <thaismca.bio@gmail.com>
2023-01-31 16:57:20 -0800

    Automated Testing * Part 1 - Hidash - Documented issues encountered in the previous implementation - Documented and implemented solution by creating a helper function to execute test cases

--- Udemy - Javascript Bootcamp/Automated Testing/Part 1 - Hidash/index.test.js
+++ Udemy - Javascript Bootcamp/Automated Testing/Part 1 - Hidash/index.test.js
@@ -1,31 +1,45 @@
 //get access to the functions in the index.js file
 const { forEach, map } = require('./index.js');
 
+//helper function to execute test cases
+//it receives a description and a function to be invoked in the arguments
+const test = (desc, func) => {
+    console.log(`----- ${desc}`);
+    try {
+        func();
+    } catch (err) {
+        console.log(err.message);
+    }
+}
+
 //testing the forEach function
 //Case 1: call a function that sums all the elements in an iterable with an array of [1, 2, 3]
 //Expected output: 6
-let sum = 0;
-forEach([1, 2, 3], value => {
-    sum += value;
+test('Testing the forEach function', () => {
+    let sum = 0;
+    forEach([1, 2, 3], value => {
+        sum += value;
+    });
+    if(sum !== 6) {
+        throw new Error('Expected summing array to equal 6');
+    }
 });
-if(sum !== 6) {
-    throw new Error('Expected summing array to equal 6');
-}
 
 //------------------------------------------------------------------------------------------------------------------
-
 //testing the map function
 //Case 1: call a function that squares each of the elements in an iterable with an array of [2, 3, 4]
 //Expected output: [4, 9, 16]
-const squares = map([2, 3, 4], value => {
-    return value * value;
+test('Testing the map function', () => {
+    const squares = map([2, 3, 4], value => {
+        return value * value;
+    });
+    if(squares[0] !== 4) {
+        throw new Error(`This result should be 4, but found ${squares[0]}`);
+    }
+    if(squares[1] !== 9) {
+        throw new Error(`This result should be 9, but found ${squares[1]}`);
+    }
+    if(squares[2] !== 16) {
+        throw new Error(`This result should be 16, but found ${squares[2]}`);
+    }
 });
-if(squares[0] !== 4) {
-    throw new Error(`This result should be 4, but found ${squares[0]}`);
-}
-if(squares[1] !== 9) {
-    throw new Error(`This result should be 9, but found ${squares[1]}`);
-}
-if(squares[2] !== 16) {
-    throw new Error(`This result should be 16, but found ${squares[2]}`);
-}
