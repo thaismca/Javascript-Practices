682cddc00a3dfc10e475a042aa37240a1dab24cc
Thais Campos <thaismca.bio@gmail.com>
2023-02-03 12:42:09 -0800

    Part 3 - Building a testing framework from scratch * TestMe Documented and implemented requirement: must be a node-based CLI framework

---
+++ Udemy - Javascript Bootcamp/Automated Testing/Part 3 - Building a testing framework from scratch/TestMe/index.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+//this line above is telling our computer we want to use Node to execute this file, as opposed to trying to execute this file directly
+
+console.log("Running TestMe......")
