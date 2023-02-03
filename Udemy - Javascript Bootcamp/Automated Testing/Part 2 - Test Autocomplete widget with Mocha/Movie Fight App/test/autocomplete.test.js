c32adcba3684d8ab4facdad0a7806e47a669c939
Thais Campos <thaismca.bio@gmail.com>
2023-02-02 22:00:44 -0800

    Part 2 - Test Autocomplete widget with Mocha * Movie Fight App Documented and implemented code to test autocomplete dropdown state after search

--- Udemy - Javascript Bootcamp/Automated Testing/Part 2 - Test Autocomplete widget with Mocha/Movie Fight App/test/autocomplete.test.js
+++ Udemy - Javascript Bootcamp/Automated Testing/Part 2 - Test Autocomplete widget with Mocha/Movie Fight App/test/autocomplete.test.js
@@ -19,6 +19,29 @@ beforeEach(() => {
     });
 });
 
+//helper function to delay test until a given element is displayed on the screen
+const waitFor = (selector) => {
+    return new Promise((resolve, reject) => {
+        const interval = setInterval(() => {
+            if (document.querySelector(selector)) {
+                //clear interval and timeout
+                clearInterval(interval);
+                clearTimeout(timeout);
+                //resolve promisse if element with the given selector is found
+                resolve();
+            }
+        }, 30);
+        const timeout = setTimeout(() => {
+            if (!document.querySelector(selector)) {
+                //clear interval
+                clearInterval(interval);
+                //reject promisse if element with the given selector is not found within 3 seconds
+                reject();
+            }
+        }, 3000);
+    }); 
+}
+
 //test dropdown state - it should be hidden when widget is first displayed
 it('Autocomplete dropdown in closed state at start', () => {
     //get a reference to the dropdown element
@@ -26,3 +49,22 @@ it('Autocomplete dropdown in closed state at start', () => {
     //check if className includes 'is-active'
     expect(dropdown.className).not.to.include('is-active');
 });
+
+//test dropdown state after search - it should be displayed when user types in the input
+it('Dropdown in active state after search', async () => {
+    //get a reference to the autocomplete text input element
+    const input = document.querySelector('input');
+    //set a value to the autocomplete text input
+    input.value = 'testing';
+    //get a fake input event to be triggered in the autocomplete text input
+    input.dispatchEvent(new Event('input'));
+
+    //delay assertion until there's a dropdown-item to be found in the document, or timeout
+    await waitFor('.dropdown-item');
+
+    //get a reference to the dropdown element
+    const dropdown = document.querySelector('.dropdown');
+    //check if className includes 'is-active'
+    expect(dropdown.className).to.include('is-active');
+
+})
