c9ac0db03f0e2ad5b923746482c2c4d6e234d579
Thais Campos <thaismca.bio@gmail.com>
2023-02-02 19:46:27 -0800

    Part 2 - Test Autocomplete widget with Mocha * Movie Fight Documented and implemented test for autocomplete dropdown state at start

--- Udemy - Javascript Bootcamp/Automated Testing/Part 2 - Test Autocomplete widget with Mocha/Movie Fight App/test/autocomplete.test.js
+++ Udemy - Javascript Bootcamp/Automated Testing/Part 2 - Test Autocomplete widget with Mocha/Movie Fight App/test/autocomplete.test.js
@@ -1,5 +1,6 @@
 //test the ability to display an autocomplete widget in the page
-it('Shows an autocomplete', () => {
+it('Autocomplete dropdown state at start', () => {
+    //create an instance of a autocomplete widget to be tested
     createAutoComplete({
         root: document.querySelector('#target'),
         fetchData() {
@@ -14,4 +15,10 @@ it('Shows an autocomplete', () => {
             return movie.Title;
         }
     });
-})
+
+    //test dropdown state - it should be hidden when widget is first displayed
+    //get a reference to the dropdown element
+    const dropdown = document.querySelector('.dropdown');
+    //check if className includes 'is-active'
+    expect(dropdown.className).not.to.include('is-active');
+});
